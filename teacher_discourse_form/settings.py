2c7a6b257b6e9840be0b139a86999f9991fa02f4
Catalina <ceinaipil@gmail.com>
2018-07-18 20:06:07 -0400

    settings change

--- teacher_discourse_form/settings.py
+++ teacher_discourse_form/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '&_-6hu!0ptgu$9ms)+(gwrf@xcv=qel5+#otms=1&_3c33m5v3'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
 
