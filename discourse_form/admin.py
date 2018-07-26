8284fc13326fab73a222c5894b782de0a1c27e32
Catalina <ceinaipil@gmail.com>
2018-07-26 13:03:19 -0400

    adding to admin view the new stuff

--- discourse_form/admin.py
+++ discourse_form/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Discourse_form,Form_answer,Answered_subject,Answered_skill,Answered_axis,Answered_learning_goal,Answered_copus_code
+from .models import Discourse_form,Form_answer,Answered_subject,Answered_skill,Answered_axis,Answered_learning_goal,Answered_copus_code,Answered_axis_phrases
 
 admin.site.register(Discourse_form)
 admin.site.register(Form_answer)
@@ -10,3 +10,4 @@ admin.site.register(Answered_skill)
 admin.site.register(Answered_axis)
 admin.site.register(Answered_learning_goal)
 admin.site.register(Answered_copus_code)
+admin.site.reigster(Answered_axis_phrases)
