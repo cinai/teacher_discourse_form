4f2de04e86dbf12c12f2a733b559e3bf95291f7c
Catalina <ceinaipil@gmail.com>
2018-07-31 11:46:36 -0400

    tryung to yoload faster

--- discourse_form/models.py
+++ discourse_form/models.py
@@ -23,7 +23,7 @@ class Form_answer(models.Model):
     dialogic = models.CharField(blank=True,default='NA',max_length=40)
     done = models.BooleanField(blank=True,default=False)
     def __str__(self):
-        label_id = str(self.pk)+'-'+self.form.artificial_name
+        label_id = str(self.form.pk)+'-'+self.form.artificial_name
         return label_id+'-'+str(self.ans_date)
 
 class Answered_subject(models.Model):
