6d3c63659d17d1c24713614af9878bdc5102caff
Catalina <ceinaipil@gmail.com>
2018-08-06 03:03:51 -0400

    shu

--- sessions_coding/fixtures/make_fixture_sessions.py
+++ sessions_coding/fixtures/make_fixture_sessions.py
@@ -14,10 +14,7 @@ les_dict = []
 
 sessions = [] # list of paths
 
-counter = 8 + (7*4)
-l_init = 81
-l_end = 101
-
+counter = 8
 for i,row in df_sessions.iterrows():
     filename = row.file[:-4] + '.txt'
     if row.resumen=='Valido' and filename in sessions_filepaths:
@@ -39,20 +36,29 @@ for i,row in df_sessions.iterrows():
             date = dt.datetime.fromtimestamp(int(row.date)/1000).isoformat()
         except ValueError:
             date = '2018-05-21'
-        a_dict = {'model':'sessions_coding.classroom_session','pk':counter,'fields':{'grade':grade,'teacher':teacher,'wav_name':name,'duration':duration,'content':content,'colegio':colegio,'date':date,'path':path}}
+        #a_dict = {'model':'sessions_coding.classroom_session','pk':counter,'fields':{'grade':grade,'teacher':teacher,'wav_name':name,'duration':duration,'content':content,'colegio':colegio,'date':date,'path':path}}
         sessions.append((counter,filename))
-        les_dict.append(a_dict)
+        #les_dict.append(a_dict)
         counter += 1
 
 from itertools import islice
 
-# load 1-20 lines
-for s in sessions:
-    file_name = os.path.join(sessions_files_path,s[1])
-    with open(file_name,'r', encoding="utf8") as f:
-        array = list(islice(f,l_init,l_end))
-    a_dict = {'model':'discourse_form.discourse_form','pk':s[0],'fields':{'session':s[0],'init_line':l_init,'end_line':l_end,'artificial_name':'_'.join([s[1],'1','20']),'text':''.join(array)}}
-    les_dict.append(a_dict)
-# output the json file
+#i = 5
+for i in [6,7,8,9,10,11,12,13,14]:
+    pk2 = 8 + (7*i)
+    l_init = 20*i + 1
+    l_end = 20*(i+1) + 1
+    # load 1-20 lines
+    for s in sessions:
+        file_name = os.path.join(sessions_files_path,s[1])
+        with open(file_name,'r', encoding="utf8") as f:
+            array = list(islice(f,l_init,l_end))
+        a_dict = {'model':'discourse_form.discourse_form','pk':pk2,'fields':{'session':s[0],'init_line':l_init,'end_line':l_end,'artificial_name':'_'.join([s[1],'1','20']),'text':''.join(array)}}
+        les_dict.append(a_dict)
+        pk2 += 1
+    # output the json file
+l_init = 20*6 + 1
+l_end = 20*(14+1) + 1
+
 with open('data_sessions_jose_'+str(l_init)+'_'+str(l_end)+'.json', 'w') as outfile:
     json.dump(les_dict, outfile)
