96d30f3556cdd15d7c425dd323ef3f88c695d9dd
Catalina <ceinaipil@gmail.com>
2018-08-09 13:34:44 -0400

    push some data

--- sessions_coding/fixtures/make_fixture_sessions.py
+++ sessions_coding/fixtures/make_fixture_sessions.py
@@ -43,22 +43,38 @@ for i,row in df_sessions.iterrows():
 
 from itertools import islice
 
-#i = 5
-for i in [6,7,8,9,10,11,12,13,14]:
-    pk2 = 8 + (7*i)
-    l_init = 20*i + 1
-    l_end = 20*(i+1) + 1
-    # load 1-20 lines
-    for s in sessions:
-        file_name = os.path.join(sessions_files_path,s[1])
+# list of segments should depend on the maximum of lines (18)
+list_of_segments = [15,16,17,18]
+n_todo_sessions = len(sessions) # the ones that are not upload
+last_pk = 8
+pk2 = last_pk + (n_todo_sessions*list_of_segments[0])
+for s in sessions:
+    file_name = os.path.join(sessions_files_path,s[1])
+    for i in list_of_segments:
+        l_init = 20*i + 1
+        l_end = 20*(i+1) + 1
         with open(file_name,'r', encoding="utf8") as f:
             array = list(islice(f,l_init,l_end))
-        a_dict = {'model':'discourse_form.discourse_form','pk':pk2,'fields':{'session':s[0],'init_line':l_init,'end_line':l_end,'artificial_name':'_'.join([s[1],'1','20']),'text':''.join(array)}}
-        les_dict.append(a_dict)
-        pk2 += 1
+    # load 1-20 lines
+        if len(array) == 0:
+            print("NO more")
+            break
+        if len(array) < 10:
+            print("INCOMPLETE "+str(file_name))
+            break
+        else:
+            a_dict = {'model':'discourse_form.discourse_form','pk':pk2,'fields':{'session':s[0],'init_line':l_init,'end_line':l_end,'artificial_name':'_'.join([s[1],'1','20']),'text':''.join(array)}}
+            les_dict.append(a_dict)
+            pk2 += 1
+
     # output the json file
-l_init = 20*6 + 1
-l_end = 20*(14+1) + 1
+l_init = 20*15 + 1
+l_end = 20*(17+1) + 1
 
 with open('data_sessions_jose_'+str(l_init)+'_'+str(l_end)+'.json', 'w') as outfile:
     json.dump(les_dict, outfile)
+
+
+for s in sessions:
+    #file_name = os.path.join(sessions_files_path,s[1])
+    print(s[1])
